8f52a40c96c0feb0f950a8bc5f294c59e95b8a89
ederson <edersonvillegas50@gmail.com>
2022-06-09 03:59:22 +0800

    add signout

--- components/Auth/SignIn.js
+++ components/Auth/SignIn.js
@@ -1,7 +1,19 @@
 import React from "react";
 import { Card, Button } from "react-bootstrap";
-
+// initialized values in the context
+import { useAuth } from "../../context/AuthProvider";
 export default function SignIn() {
+  // use signIn function from context
+  const { signin } = useAuth();
+
+  const handleSignIn = async () => {
+    try {
+      await signin();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <Card border="light">
       <Card.Body>
@@ -10,7 +22,7 @@ export default function SignIn() {
           Github user to use the application and view the weather in your city.
         </Card.Text>
 
-        <Button>
+        <Button onClick={handleSignIn}>
           <i className="fa fa-github" aria-hidden="true"></i> Github
         </Button>
       </Card.Body>
