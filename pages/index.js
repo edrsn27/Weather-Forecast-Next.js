f91dd249ae1515326a2b56949186f3a8fbf4cc40
ederson <edersonvillegas50@gmail.com>
2022-06-09 04:33:55 +0800

    check if user is auth on index page for SignIn and Profile Components

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,11 @@ import AuthLayout from "../layouts/Auth";
 import SignIn from "../components/Auth/SignIn";
 import UserProfile from "../components/Main/UserProfile";
 import { Container } from "react-bootstrap";
-
+import { useAuth } from "../context/AuthProvider";
 export default function Index() {
+  const { currentUser, loading } = useAuth();
 
+  if(loading) return <div>Loading...</div>;
   return (
     <AuthLayout>
       <Container
@@ -13,8 +15,8 @@ export default function Index() {
         style={{ minHeight: "200px" }}
       >
         <div className="w-100 " style={{ maxWidth: "700px" }}>
-          <UserProfile />
-          <SignIn />
+          {currentUser && <UserProfile />}
+          {!currentUser && <SignIn />}
         </div>
       </Container>
     </AuthLayout>
