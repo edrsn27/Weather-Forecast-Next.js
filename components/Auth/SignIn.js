6c062b75391d94603ef94a132e92495b47df5999
ederson <edersonvillegas50@gmail.com>
2022-06-09 02:48:26 +0800

    initial sign-in page

---
+++ components/Auth/SignIn.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Card, Button } from "react-bootstrap";
+
+import Image from "next/image";
+
+export default function SignIn() {
+  return (
+    <Card border="light">
+      <Card.Body>
+        <Card.Text>
+          Welcome to weather forecast web application. Please login with your
+          Github user to use the application and view the weather in your city.
+        </Card.Text>
+      </Card.Body>
+    </Card>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,18 @@
 import React from "react";
 import AuthLayout from "../layouts/Auth";
+import SignIn from "../components/Auth/SignIn";
+import { Container } from "react-bootstrap";
 export default function Index() {
   return (
     <AuthLayout>
-      <div>index</div>
+      <Container
+        className="h-100 d-flex align-items-center justify-content-center"
+        style={{ minHeight: "200px" }}
+      >
+        <div className="w-100 " style={{ maxWidth: "700px" }}>
+          <SignIn />
+        </div>
+      </Container>
     </AuthLayout>
   );
 }
