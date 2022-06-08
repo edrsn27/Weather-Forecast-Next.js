212220f719dd0a1d4cce4764993bfa57b09ac24a
ederson <edersonvillegas50@gmail.com>
2022-06-09 03:24:23 +0800

    Create sign-in for github in AuthProvider

---
+++ context/AuthProvider.js
@@ -0,0 +1,45 @@
+import { createContext, useContext, useState, useEffect } from "react";
+// get auth token from local storage
+import { auth } from "../firebase-config";
+// initialize github auth provider
+import { GithubAuthProvider, signInWithPopup } from "firebase/auth";
+// set github auth provider
+const provider = new GithubAuthProvider();
+// create context
+const AuthContext = createContext();
+// export context
+export const useAuth = () => {
+  return useContext(AuthContext);
+};
+
+export default function AuthProvider({ children }) {
+  const [currentUser, setCurrentUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  // function for sign in with github
+  const signin = () => {
+    return signInWithPopup(auth, provider);
+  };
+
+  useEffect(() => {
+    // check if user is signed in
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      if (user) {
+        user.getIdTokenResult().then((idTokenResult) => {
+          user.admin = idTokenResult.claims.admin;
+        });
+      }
+      // set current user
+      setCurrentUser(user);
+      setLoading(false);
+    });
+
+    return unsubscribe;
+  }, []);
+
+  const value = {
+    currentUser,
+    loading,
+    signin,
+  };
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+}
