580c974e4f03bf562be9be778d35ea8aab138eb4
ederson <edersonvillegas50@gmail.com>
2022-06-09 04:25:39 +0800

    show github link

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
 import React from "react";
 import AuthLayout from "../layouts/Auth";
 import SignIn from "../components/Auth/SignIn";
+import UserProfile from "../components/Main/UserProfile";
 import { Container } from "react-bootstrap";
+
 export default function Index() {
+
   return (
     <AuthLayout>
       <Container
@@ -10,6 +13,7 @@ export default function Index() {
         style={{ minHeight: "200px" }}
       >
         <div className="w-100 " style={{ maxWidth: "700px" }}>
+          <UserProfile />
           <SignIn />
         </div>
       </Container>
