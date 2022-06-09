47f4ef330c081523c9f22c62dfd7616816a993d4
ederson <edersonvillegas50@gmail.com>
2022-06-09 08:10:55 +0800

    show weather table

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,14 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "font-awesome/css/font-awesome.min.css";
 import AuthProvider from "../context/AuthProvider";
-
+import WeatherProvider from "../context/WeatherProvider";
 function MyApp({ Component, pageProps }) {
   return (
     // AuthProvider is the context provider for the application allow to access values from the context
     <AuthProvider>
-      <Component {...pageProps} />
+      <WeatherProvider>
+        <Component {...pageProps} />
+      </WeatherProvider>
     </AuthProvider>
   );
 }
